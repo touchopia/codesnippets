74e6a7857c91f57b4ff0f55ab4280bc7727d6284
Phil Wright <touchopia@me.com>
2015-08-18 17:07:29 -0600

    Imported user code snippets

---
+++ pageviewdatasource.m
@@ -0,0 +1,16 @@
+// pageViewDataSource
+// 
+//
+// IDECodeSnippetCompletionPrefix: tlpagedata
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: E661DA2F-69E1-4CA7-9C22-2D8F71025BFB
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
---
+++ viewdidload.m
@@ -0,0 +1,17 @@
+// viewDidLoad
+// 
+//
+// IDECodeSnippetCompletionPrefix: tlviewdefault
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: CC51FB6E-40E8-4783-BD42-A013C1BAC0D4
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
+// IDECodeSnippetPlatformFamily: iphoneos
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
